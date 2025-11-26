1a271028724fde8fd1b2d15c92a4aa86959c7c51
“jundalisay” <jundalisay@yahoo.com>
2025-11-26 15:38:41 +0800

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,11 @@
 </script>
 
 <div class="min-h-screen bg-gray-50 pb-20">
-  <header class="bg-blue-600 text-white p-4 sticky top-0 z-10">
-    <h1 class="text-xl font-bold">Circle App</h1>
-    {#if data.user}
-      <p class="text-sm">Welcome, {data.user.name}</p>
-    {/if}
-  </header>
-  
   <main class="container mx-auto px-4 py-6 max-w-7xl">
     {@render children()}
   </main>
   
   {#if data.user && $page.route.id !== '/menu'}
-    <FAB />
+    <FAB user={data.user} />
   {/if}
 </div>
