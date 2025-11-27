119a8c6ba37afd1d6e2edc11b1f7739582531078
“pantrypoints” <pantrypoints@gmail.com>
2025-11-27 20:35:37 +0800

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,39 +1,16 @@
-// import type { Config } from 'drizzle-kit';
-
-// export default {
-//   schema: './src/lib/server/db/schema.ts',
-//   out: './drizzle',
-//   dialect: 'sqlite',
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL || 'file:local.db'
-//   }
-// } satisfies Config;
-
-
 import { defineConfig } from 'drizzle-kit';
-
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'turso',
-	dbCredentials: {
-		authToken: process.env.DATABASE_AUTH_TOKEN,
-		url: process.env.DATABASE_URL
-	},
-	verbose: true,
-	strict: true
+  schema: './src/lib/server/db/schema.ts',
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN
+  },
+
+  verbose: true,
+  strict: true,
+  dialect: 'turso'
 });
-
-
-// export default {
-//   schema: './src/lib/server/db/schema.ts',
-//   out: './drizzle',
-//   dialect: 'sqlite',
-//   dbCredentials: {
-//     url: 'file:local.db'
-//   }
-// } satisfies Config;
-
-
 
